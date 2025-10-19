159fbd2e2b663089951c6f37cf412cd9543cdf7b
Gleb Lobov <lobzison@gmail.com>
2025-10-19 15:58:05 +0200

    Stop getting stuck

--- src/main/scala/nyetbot/functionality/LlmFunctionality.scala
+++ src/main/scala/nyetbot/functionality/LlmFunctionality.scala
@@ -68,7 +68,8 @@ class LlmFunctionalityImpl(
             _               = println(s"predicted bot message: $replyEng")
             reply          <- translationService.translate(replyEng, TranslationService.TargetLang.RU)
             replyMsg       <- IO.pure(LlmContextMessage(config.userPrefix + config.botName, reply))
-            _              <- contextRef.update(msgs => (msgs :+ replyMsg).takeRight(20))
+            // disable self messages for now
+            // _              <- contextRef.update(msgs => (msgs :+ replyMsg).takeRight(20))
             _              <- sendIfNotEmpty(reply.trim)
         yield ()
 
